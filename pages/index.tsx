81e0fe59289b60c92cc9dbaa2b77575adbbc0305
Raul Correia <raulvinicius245@gmail.com>
2022-08-11 15:55:36 -0300

    Adicionando cores

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,14 @@ const HomeNoAuth = () => {
   return (
     <>
       <Head>
-        <title>Home</title>
+        <title>Onebitflix</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+        <meta property="og:title" content="Onebitflix" key="title" />
+        <meta name="description" 
+        content="
+        Tenha acesso aos melhores conteudos
+        de programação de uma forma simples e fácil!
+        "/>
       </Head>
       <main></main>
     </>
