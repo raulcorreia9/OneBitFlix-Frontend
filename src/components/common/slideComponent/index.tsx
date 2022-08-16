7f685d0e6661ebd7b3d924eaef1a63ca9938a061
Raul Correia <raulvinicius245@gmail.com>
2022-08-16 14:38:20 -0300

    Responsividade para a seção de slides

--- src/components/common/slideComponent/index.tsx
+++ src/components/common/slideComponent/index.tsx
@@ -8,15 +8,42 @@ interface props {
 }
 
 const SlideComponent = ({ course }: props) => {
+    let slideCount = 0;
+    console.log(course.length);
+    if(course.length > 4) {
+        slideCount = 4;
+    } else {
+        slideCount = course.length;
+    }
+
     return (
         <>
             <div className="d-flex flex-column align-items-center py-4">
                 <Splide options={{
                     type:"loop",
-                    perPage: 4,
+                    perPage: slideCount,
                     perMove: 1,
-                    width: 1200,
+                    width: slideCount * 300,
                     pagination: false,
+                    arrows: course.length > 4 ? true : false,
+                    drag: course.length > 4 ? true : false,
+                    breakpoints: {
+                        1200: {
+                            perPage: slideCount >= 2 ? 2 : 1,
+                            width: slideCount >= 2 ? 600 : 300,
+                            arrows: course.length > 2 ? true : false,
+                            drag: course.length > 2 ? true : false,
+                        },
+                        600: {
+                            perPage: 1,
+                            width: 300,
+                            arrows: course.length > 1 ? true : false,
+                            drag: course.length > 1 ? true : false,
+                        },
+                        300: {
+                            width: 270
+                        }
+                    }
                     }}>        
                     { course?.map((course) => (
                         <SplideSlide key={ course.id }>
