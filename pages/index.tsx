3f5b391c7b8f29e45b49b45e1837c2b2c8667105
Raul Correia <raulvinicius245@gmail.com>
2022-08-16 15:54:53 -0300

    Animações para pagina principal com AOS

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,18 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 import { GetStaticProps } from "next";
 import Head from "next/head";
+import AOS from "aos";
+import "aos/dist/aos.css";
 //Components
 import HeaderNoAuth from "../src/components/homeNoAuth/headerNoAuth";
 import PresentationSection from "../src/components/homeNoAuth/presentationSection";
 import CardsSection from "../src/components/homeNoAuth/cardsSection";
 import SlideSection from "../src/components/homeNoAuth/slideSection";
+import Footer from "../src/components/common/footer";
 //Style
 import styles from "../styles/HomeNoAuth.module.scss";
 //Services
 import courseService, { CourseType } from "../src/services/courseService";
-import Footer from "../src/components/common/footer";
 
 interface IndexPageProps {
   chrildren?: ReactNode;
@@ -18,6 +20,11 @@ interface IndexPageProps {
 } 
 
 const HomeNoAuth = ({ course }: IndexPageProps) => {
+  
+  useEffect(() => {
+    AOS.init();
+  }, [])
+  
   return (
     <>
       <Head>
@@ -31,13 +38,20 @@ const HomeNoAuth = ({ course }: IndexPageProps) => {
         "/>
       </Head>
       <main>
-        <div className={ styles.sectionBackground }>
+        <div className={ styles.sectionBackground } 
+          data-aos="fade-zoom-in" 
+          data-aos-duration="1600"
+        >
           <HeaderNoAuth />
           <PresentationSection />
         </div>
+        <div data-aos="fade-right" data-aos-duration="1200">
           <CardsSection />
-          <SlideSection newestCourses={ course }/>
-          <Footer />
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1350">
+          <SlideSection newestCourses={ course } />
+        </div>
+        <Footer />
       </main>
     </>
   );
