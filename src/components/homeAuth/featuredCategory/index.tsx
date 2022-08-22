37d1031ee4b32fbe9534a3fd37cdedd302b20929
Raul Correia <raulvinicius245@gmail.com>
2022-08-22 16:11:01 -0300

    Categoria para destaques

---
+++ src/components/homeAuth/featuredCategory/index.tsx
@@ -0,0 +1,25 @@
+import useSWR from 'swr';
+import styles from '../../../../styles/slideCategory.module.scss';
+import courseService from '../../../services/courseService';
+import SlideComponent from '../../common/slideComponent';
+
+const FeaturedCategory = function () {
+    const { data, error } = useSWR("/featured", courseService.getFavCourses);
+    
+    if(error) {
+        return error;
+    }
+
+    if(!data) {
+        return (<><p>Loading...</p></>)
+    }
+    
+    return (
+        <>
+            <p className={ styles.titleCategory }>EM DESTAQUE</p>
+            <SlideComponent course={ data.data } />
+        </>
+    );
+};
+
+export default FeaturedCategory;
