25eb83d29d359d5afe6b7a22c7c8397b5b5a63de
Raul Correia <raulvinicius245@gmail.com>
2022-10-12 22:30:58 -0300

    Iniciado Profile page: Form de informações pessoais e mudança de senha

---
+++ pages/profile.tsx
@@ -0,0 +1,39 @@
+import Head from "next/head";
+import { Button, Col, Container, Row } from "reactstrap";
+//Components
+import HeaderAuth from "../src/components/common/headerAuth";
+import UserForm from "../src/components/profile/user";
+
+import styles from "../styles/profile.module.scss";
+
+const UserInfo = () => {
+    return (
+        <>
+            <Head>
+                <title>Onebitflix - Meus dados</title>
+                <link rel="shortcut icon" href="/favicon.svg" type="image/x-icon" />
+            </Head>
+            <main>
+                <HeaderAuth />
+                <Container className="py-5">
+                    <p className={ styles.title }>Minha Conta</p>
+                    <Row className="pt-3 pb-5">
+                        <Col md={4} className={ styles.btnColumn }>
+                            <Button className={ styles.renderForm }>
+                                DADOS PESSOAIS
+                            </Button>
+                            <Button className={ styles.renderForm }>
+                                SENHA
+                            </Button>
+                        </Col>
+                        <Col md>
+                            <UserForm />
+                        </Col>
+                    </Row>
+                </Container>
+            </main>
+        </>
+    )
+}
+
+export default UserInfo;
---
+++ src/components/profile/user/index.tsx
@@ -0,0 +1,97 @@
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import styles from '../../../../styles/profile.module.scss';
+
+const UserForm = () => {
+    return(
+        <>
+            <Form className={ styles.form }>
+                <div className={ styles.formName }>
+                    <p className={ styles.nameAbbreviation }>NT</p>
+                    <p className={ styles.userName }>NAME TEST</p>
+                </div>
+                <div className={ styles.memberTime }>
+                    <img 
+                        src="/profile/iconUserAccount.svg"
+                        alt="Icon Profile"
+                        className={ styles.memberTimeImg }
+                    />
+                    <p className={ styles.memberTimeText }>Membro desde <br /> xx de xxxx de xxxx</p>
+                </div>
+                <hr />
+                <div>
+                    <FormGroup>
+                        <Label for="firstName" className={ styles.label }>
+                            NOME
+                        </Label>                    
+                        <Input
+                            name="firstName"
+                            type="text"
+                            id="firstName"
+                            placeholder="Qual o seu primeiro nome?"
+                            required
+                            maxLength={20}
+                            className={ styles.inputFlex }
+                            value={"NAME"}
+                        >
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="lastName" className={ styles.label }>
+                            SOBRENOME
+                        </Label>                    
+                        <Input
+                            name="lastName"
+                            type="text"
+                            id="lastName"
+                            placeholder="Qual o seu último nome?"
+                            required
+                            maxLength={20}
+                            className={ styles.inputFlex }
+                            value={"TEST"}
+                        >
+                        </Input>
+                    </FormGroup>
+                </div>
+                <div>
+                    <FormGroup>
+                        <Label for="phone" className={ styles.label }>
+                            WHATSAPP/TELEGRAM
+                        </Label>                    
+                        <Input
+                            name="phone"
+                            type="tel"
+                            id="phone"
+                            placeholder="(xx) 9xxxx-xxxx"
+                            required
+                            maxLength={20}
+                            className={ styles.inputFlex }
+                            value={"+55 (21) 99999-9999"}
+                        >
+                        </Input>
+                    </FormGroup>
+                    <FormGroup>
+                        <Label for="email" className={ styles.label }>
+                            E-MAIL
+                        </Label>                    
+                        <Input
+                            name="email"
+                            type="email"
+                            id="email"
+                            placeholder="Qual o seu e-mail?"
+                            required
+                            maxLength={20}
+                            className={ styles.inputFlex }
+                            value={"jacare@email.com"}
+                        >
+                        </Input>
+                    </FormGroup>
+                </div>
+                <Button className={ styles.formBtn } outline type="submit">
+                    Salvar Alterações
+                </Button>
+            </Form>
+        </>
+    )
+}
+
+export default UserForm;
